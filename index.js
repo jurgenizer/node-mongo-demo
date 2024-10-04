e32190b133ff567bd4f0671fdd61ec429c537e27
Jurgen <jurgen.geitner@gmail.com>
2024-10-04 16:10:41 +0200

    created a course model

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ const courseSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 });
+
+// Compile schema into a model
+// PascalCase to name our classes, e.g., Course
+const Course = mongoose.model('Course', courseSchema);
+// camelCase to name our obbjects, e.g., course
+const course = new Course({
+    name: 'Node.js Course',
+    author: 'Jurgen',
+    tags: ['node', 'backend'],
+    isPublished: true
+})
